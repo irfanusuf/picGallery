8cc2af5ea85512311038f4d676440e268db7362a
unknown <irfanusuf33@gmail.com>
2024-06-25 15:48:49 +0530

    fixes

---
+++ New folder/index.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", () => {
+  async function fetchVideos(query) {
+    try {
+      const apikey = "EbvixQkEPE7kqKI2qHd0ackQCu5roaHHF5jiJvSykgsxxKTcf80F2vKY";
+      const url = `https://api.pexels.com/videos/search?query=${query}&per_page=20`;
+      const options = {
+        method: "GET",
+        headers: {
+          Authorization: apikey,
+        },
+      };
+
+      const result = await fetch(url, options);
+      const data = await result.json();
+      console.log(data);
+
+      const videoArr = data.videos
+
+      displayVideos(videoArr)
+
+ 
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function displayVideos(videoArr) {
+    const targetDiv = document.getElementById("video-card");
+    targetDiv.innerHTML = "";
+
+    videoArr.forEach((video) => {
+      const videoElement = document.createElement("div");
+      videoElement.classList.add("video-container");
+
+      const videoPlayer = document.createElement("video");
+      videoPlayer.src = video.video_files[0].link;
+      videoPlayer.controls = true;
+      videoPlayer.muted = true;
+
+      targetDiv.appendChild(videoElement);
+      videoElement.appendChild(videoPlayer);
+    });
+  }
+
+  document.getElementById("btn").addEventListener("click", () => {
+    const x = document.getElementById("something").value;
+    fetchVideos(x);
+  });
+});
---
+++ instasearch.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+  async function instaSearch(query) {
+    // const url = `https://instagram-scraper-api2.p.rapidapi.com/v1/search_users?search_query= ${query}`;
+
+
+  const url = `https://instagram-scraper-api2.p.rapidapi.com/v1/posts?username_or_id_or_url=${query}`;
+
+
+
+
+    const options = {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "79bd850338msh2791f9644fac656p197751jsnac6970a86c03",
+        "x-rapidapi-host": "instagram-scraper-api2.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.text();
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+
+instaSearch("photogram.kashmir")
+
+
+
+});
